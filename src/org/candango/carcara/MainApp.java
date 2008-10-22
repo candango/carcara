6b1ea13050c4991fb850e2f2e8213d9501eb0038
Flavio Garcia <piraz@candango.org>
2008-10-22 02:21:24 +0000

    Main app testing dao builders

--- src/org/candango/carcara/MainApp.java
+++ src/org/candango/carcara/MainApp.java
@@ -1,5 +1,10 @@
 package org.candango.carcara;
 
+import org.candango.carcara.engine.AbstractDatabaseLoader;
+import org.candango.carcara.engine.DaoBuilder;
+import org.candango.carcara.engine.DatabaseLoader;
+import org.candango.carcara.engine.mysql.MysqlDatabaseConfiguration;
+import org.candango.carcara.engine.mysql.MysqlDaoBuilder;
 import org.candango.carcara.ui.MainFrame;
 
 public class MainApp {
@@ -7,7 +12,35 @@ public class MainApp {
 	public static void main( String[] args ) {
 		
 		
-		MainFrame frame = new MainFrame();
+		//MainFrame frame = new MainFrame();
+		
+		DatabaseLoader loader = AbstractDatabaseLoader.getLoader( 
+				
+				AbstractDatabaseLoader.MYSQL_DATABASE );
+		
+		MysqlDatabaseConfiguration configuration = (MysqlDatabaseConfiguration)loader.getConfiguration();
+		
+		//configuration.setSchema( "intranet" );
+		
+		configuration.setHost( "localhost" );
+		
+		configuration.setDatabase( "carlinhosveiga" );
+		
+		configuration.setUser( "root" );
+		
+		configuration.setPassword( "rootpass" );
+		
+		loader.connect( configuration );
+		
+		loader.doLoad( configuration );
+		
+		loader.disconnect();
+		
+		DaoBuilder builder = new MysqlDaoBuilder();
+		
+		builder.setPath( "./" );
+		
+		builder.build( configuration, loader );
 		
 	}
 	
