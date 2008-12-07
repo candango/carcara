901a7721d97a2db2a1e868e2b33a18b7fc6c8fd5
Flavio Garcia <piraz@candango.org>
2008-12-07 02:41:23 +0000

    Adding project interface. Refers #6.

---
+++ src/org/candango/carcara/model/project/Project.java
@@ -0,0 +1,52 @@
+/* Project - Project.java
+ * 
+ * This is the Carcara project interface. Defines behavior and how project will
+ * work into application.
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * This product includes software developed by the Fusebox Corporation 
+ * (http://www.fusebox.org/).
+ * 
+ * The Original Code is Carcara "a Candango Modelling tool to create 
+ * myFuses/iflux applications" part .
+ * 
+ * The Initial Developer of the Original Code is Flavio Goncalves Garcia.
+ * Portions created by Flavio Goncalves Garcia are Copyright (C) 2008 - 2009.
+ * All Rights Reserved.
+ * 
+ * Contributor(s): Flavio Goncalves Garcia.
+ */
+package org.candango.carcara.model.project;
+
+/**
+ * Project - Project.java
+ * 
+ * This is the Carcara project interface. Defines behavior and how project will
+ * work into application.
+ *
+ * @author     Flavio Goncalves Garcia <flavio.garcia@candango.org>
+ * @copyright  Copyright (c) 2008 - 2009 Candango Open Source Group
+ * @link       http://www.candango.org/myfuses
+ * @license    http://www.mozilla.org/MPL/MPL-1.1.html  MPL 1.1
+ * @version    SVN: $Id$
+ */
+public interface Project {
+	
+	public String getName();
+	
+	public void setName( String name );
+	
+	public String getProjectPackage();
+	
+	public void setProjectPackage( String pack );
+	
+}
