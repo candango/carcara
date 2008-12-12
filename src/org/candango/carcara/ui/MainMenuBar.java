881deeac2b31e37957b9647751c701072b6cc054
Flavio Garcia <piraz@candango.org>
2008-12-12 00:21:15 +0000

    Adding header comments.

--- src/org/candango/carcara/ui/MainMenuBar.java
+++ src/org/candango/carcara/ui/MainMenuBar.java
@@ -1,3 +1,29 @@
+/* MainMenuBar - MainMenuBar.java
+ * 
+ * Carcara application main menu bar. Contains all commands of application.
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * This product includes software developed by the Fusebox Corporation 
+ * (http://www.fusebox.org/).
+ * 
+ * The Original Code is Carcara "a Candango Modelling tool to create 
+ * myFuses/iflux applications" part .
+ * 
+ * The Initial Developer of the Original Code is Flavio Goncalves Garcia.
+ * Portions created by Flavio Goncalves Garcia are Copyright (C) 2008 - 2009.
+ * All Rights Reserved.
+ * 
+ * Contributor(s): Flavio Goncalves Garcia.
+ */
 package org.candango.carcara.ui;
 
 import java.awt.event.ActionEvent;
@@ -15,6 +41,15 @@ import javax.swing.KeyStroke;
 import org.candango.carcara.MainApp;
 import org.candango.carcara.model.project.Project;
 
+/**
+ * Carcara application main menu bar. Contains all commands of application.
+ *
+ * @author     Flavio Goncalves Garcia <flavio.garcia at candango.org>
+ * @copyright  Copyright (c) 2008 - 2009 Candango Open Source Group
+ * @link       http://www.candango.org/myfuses
+ * @license    http://www.mozilla.org/MPL/MPL-1.1.html  MPL 1.1
+ * @version    SVN: $Id: Project.java 23 2008-12-07 02:54:38Z flavio.garcia $
+ */
 public class MainMenuBar extends JMenuBar implements ActionListener {
 
 	/**
@@ -118,7 +153,6 @@ public class MainMenuBar extends JMenuBar implements ActionListener {
 				int wantExit = JOptionPane.showConfirmDialog( frame , 
 						"You have non saved projets. Do you really want " + 
 						"to close the application?" );
-				
 				if( wantExit == 0 ) {
 					System.exit( 0 );
 				}
@@ -217,3 +251,4 @@ public class MainMenuBar extends JMenuBar implements ActionListener {
 	}
 	
 }
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
