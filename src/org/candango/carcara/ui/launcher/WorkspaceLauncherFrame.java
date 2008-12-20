914832e1d86104f86d5a666f7156b8bedec00fba
Flavio Garcia <piraz@candango.org>
2008-12-20 01:50:41 +0000

    Adding header comments.

--- src/org/candango/carcara/ui/launcher/WorkspaceLauncherFrame.java
+++ src/org/candango/carcara/ui/launcher/WorkspaceLauncherFrame.java
@@ -1,3 +1,30 @@
+/* WorkspaceLauncherFrame - WorkspaceLauncherFrame.java
+ * 
+ * This screen handles the workspace creating and selection and launches the 
+ * selected in the workbench.
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * This product includes software developed by the Fusebox Corporation 
+ * (http://www.fusebox.org/).
+ * 
+ * The Original Code is Carcara "a Candango Modeling tool to create 
+ * myFuses/iflux applications" part .
+ * 
+ * The Initial Developer of the Original Code is Flavio Goncalves Garcia.
+ * Portions created by Flavio Goncalves Garcia are Copyright (C) 2008 - 2009.
+ * All Rights Reserved.
+ * 
+ * Contributor(s): Flavio Goncalves Garcia.
+ */
 package org.candango.carcara.ui.launcher;
 
 import java.awt.BorderLayout;
@@ -19,6 +46,16 @@ import javax.swing.JPanel;
 import javax.swing.JSeparator;
 import javax.swing.JTextArea;
 
+/**
+ * This screen handles the workspace creating and selection and launches the 
+ * selected in the workbench.
+ *
+ * @author     Flavio Goncalves Garcia <flavio.garcia at candango.org>
+ * @copyright  Copyright (c) 2008 - 2009 Candango Open Source Group
+ * @link       http://www.candango.org/myfuses
+ * @license    http://www.mozilla.org/MPL/MPL-1.1.html  MPL 1.1
+ * @version    SVN: $Id: Project.java 23 2008-12-07 02:54:38Z flavio.garcia $
+ */
 public class WorkspaceLauncherFrame extends JFrame {
 	
 	/**
@@ -85,7 +122,8 @@ public class WorkspaceLauncherFrame extends JFrame {
 		pane.add( selectWorkspaceLabel, c );
 		
 		// creating workspace info text area
-		JTextArea workspaceInfoTextArea = new JTextArea( "   Carcara Modelling Tool stores your " + 
+		JTextArea workspaceInfoTextArea = new JTextArea( 
+				"   Carcara Modeling Tool stores your " + 
 				"projects in a folder called a workspace.\n   " + 
 				"Choose a workspace folder to use for this session." );
 		
@@ -183,7 +221,8 @@ public class WorkspaceLauncherFrame extends JFrame {
 		
 		c.insets = new Insets(2, 2, 14, 2);
 		
-		JCheckBox defaultWorkspaceCheckBox = new JCheckBox( "Use this as the default and do not ask again" );
+		JCheckBox defaultWorkspaceCheckBox = new JCheckBox( 
+				"Use this as the default and do not ask again" );
 		
 		Font font1 = new Font( defaultWorkspaceCheckBox.getFont().getName(), 
 				Font.PLAIN, defaultWorkspaceCheckBox.getFont().getSize() );
@@ -228,3 +267,4 @@ public class WorkspaceLauncherFrame extends JFrame {
 	}
 	
 }
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
