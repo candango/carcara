d083461391ac44151599a05341d5ef6ee276789d
Flavio Garcia <piraz@candango.org>
2008-12-21 23:58:30 +0000

    Adding workspace reference class. Refers #11.

---
+++ src/org/candango/carcara/model/environment/WorkspaceReference.java
@@ -0,0 +1,11 @@
+package org.candango.carcara.model.environment;
+
+public interface WorkspaceReference {
+	
+	public String getPath();
+	
+	public String setPath( String path );
+	
+	public boolean isDefault();
+	
+}
