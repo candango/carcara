7ff0547da270963748199944ba358c7a0f564eef
Flavio Garcia <piraz@candango.org>
2009-01-14 13:02:00 +0000

    Implementing Workspace interface basic methods. Refers #13.

--- src/org/candango/carcara/model/environment/BasicWorkspace.java
+++ src/org/candango/carcara/model/environment/BasicWorkspace.java
@@ -1,25 +1,32 @@
 package org.candango.carcara.model.environment;
 
+import java.util.ArrayList;
+
 import org.candango.carcara.model.project.Project;
 
 public class BasicWorkspace implements Workspace {
 	
+	private  ArrayList< Project > projectList = new ArrayList<Project>(); 
+	
 	@Override
 	public void addProject(Project project) {
-		// TODO Auto-generated method stub
-
+		projectList.add( project );
 	}
 
 	@Override
 	public Project getProject(String name) {
-		// TODO Auto-generated method stub
+		for( Project project : projectList ) {
+			if( project.getName().equals( name ) ) {
+				return project;
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public Project[] getProjects() {
-		// TODO Auto-generated method stub
-		return null;
+		Project[] projects = new Project[ projectList.size() ];
+		return projectList.toArray( projects );
 	}
 
 }
