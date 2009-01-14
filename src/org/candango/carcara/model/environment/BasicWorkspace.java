e765eb1b4fc100b8edc0a68b35fe81b61f8a311e
Flavio Garcia <piraz@candango.org>
2009-01-14 12:58:10 +0000

    Adding BasicWorkspace class. Refers #13.

---
+++ src/org/candango/carcara/model/environment/BasicWorkspace.java
@@ -0,0 +1,25 @@
+package org.candango.carcara.model.environment;
+
+import org.candango.carcara.model.project.Project;
+
+public class BasicWorkspace implements Workspace {
+	
+	@Override
+	public void addProject(Project project) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Project getProject(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Project[] getProjects() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
