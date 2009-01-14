172484571d985af62dd530cd85d43d3632d128cf
Flavio Garcia <piraz@candango.org>
2009-01-14 12:56:35 +0000

    Adding basic workspace methods. Refers #13.

--- src/org/candango/carcara/model/environment/Workspace.java
+++ src/org/candango/carcara/model/environment/Workspace.java
@@ -1,5 +1,13 @@
 package org.candango.carcara.model.environment;
 
-public interface Workspace {
+import org.candango.carcara.model.project.Project;
 
+public interface Workspace {
+	
+	public Project getProject( String name );
+	
+	public Project[] getProjects();
+	
+	public void addProject( Project project );
+	
 }
