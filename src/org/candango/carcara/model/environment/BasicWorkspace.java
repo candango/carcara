a14f142bb6e78a577b8131c86c0398d174d9e7f9
Flavio Garcia <piraz@candango.org>
2009-01-14 16:16:48 +0000

    Adding conf xml save. Refers #13.

--- src/org/candango/carcara/model/environment/BasicWorkspace.java
+++ src/org/candango/carcara/model/environment/BasicWorkspace.java
@@ -6,13 +6,22 @@ import org.candango.carcara.model.project.Project;
 
 public class BasicWorkspace implements Workspace {
 	
+	/**
+	 * Array list that stores projects
+	 */
 	private  ArrayList< Project > projectList = new ArrayList<Project>(); 
 	
+	/* (non-Javadoc)
+	 * @see org.candango.carcara.model.environment.Workspace#addProject(org.candango.carcara.model.project.Project)
+	 */
 	@Override
 	public void addProject(Project project) {
 		projectList.add( project );
 	}
 
+	/* (non-Javadoc)
+	 * @see org.candango.carcara.model.environment.Workspace#getProject(java.lang.String)
+	 */
 	@Override
 	public Project getProject(String name) {
 		for( Project project : projectList ) {
@@ -23,10 +32,32 @@ public class BasicWorkspace implements Workspace {
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see org.candango.carcara.model.environment.Workspace#getProjects()
+	 */
 	@Override
 	public Project[] getProjects() {
 		Project[] projects = new Project[ projectList.size() ];
 		return projectList.toArray( projects );
 	}
-
+	
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString() {
+		String out = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+		out += "<workspace>\n";
+		out += "\t<project>\n";
+		for( Project project : projectList ) {
+			out += "\t\t<project >\n";
+			out += "\t\t\t<name>" + project.getName() + "</name>\n";
+			out += "\t\t\t<package>" + project.getProjectPackage() + "</package>\n";
+			out += "\t\t</project >\n";
+		}
+		out += "\t</project>\n";
+		out += "</workspace>";
+		return out;
+	}
+	
 }
