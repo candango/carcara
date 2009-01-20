29b4c7e83e2f2f2401e9ca16ce5f1bd88c7bd7e0
Flavio Garcia <piraz@candango.org>
2009-01-20 11:46:49 +0000

    Removing all project methods. Refers #7 #15.

--- src/org/candango/carcara/MainApp.java
+++ src/org/candango/carcara/MainApp.java
@@ -53,11 +53,6 @@ import org.candango.carcara.model.project.Project;
  */
 public class MainApp {
 	
-	/**
-	 * Project map. Store all project that application will handle.
-	 */
-	private static HashMap<String, Project> projectMap = 
-		new HashMap<String, Project>();
 	
 	private static MainFrame mainFrame;
 	
@@ -91,89 +86,6 @@ public class MainApp {
 		
 		configureEnvironment();
 	
-	}
-	
-	/**
-	 * Return all projects stored in application
-	 * 
-	 * @return An array of projects
-	 */
-	public static Project[] getProjects() {
-		
-		Project[] projects = new Project[ projectMap.size() ];
-		
-		int i = 0;
-		
-		for( String key : projectMap.keySet() ){
-			projects[ i ] = projectMap.get( key );
-			i++;
-		}
-		
-		return projects;
-	}
-	
-	/**
-	 * Add one project into application control.
-	 * 
-	 * @param key
-	 * @param project
-	 */
-	public static void addProject( String key, Project project ) {
-		projectMap.put( key , project );
-	}
-	
-	/**
-	 * Return one project by a given key
-	 * 
-	 * @param key
-	 * @return One project
-	 */
-	public static Project getProject( String key ) {
-		return projectMap.get( key );
-	}
-	
-	public static boolean hasProject( String key ) {
-		return projectMap.containsKey( key );
-	}
-	
-	/**
-	 * Create one project by a given key
-	 * 
-	 * @param key
-	 */
-	public static void createProject( String key  ) {
-		
-		if( hasProject( key ) ){
-			JOptionPane.showMessageDialog(mainFrame, "A project with name \"" + 
-					key + "\" already exists in the workspace.", 
-					"Creating Project", JOptionPane.ERROR_MESSAGE );
-		}
-		else {
-			Project project = null;
-			
-		    try {
-				project = (Project) Class.forName( 
-						"org.candango.carcara.php.Project" ).newInstance();
-				
-			} catch (ClassNotFoundException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}catch (InstantiationException e2) {
-				// TODO Auto-generated catch block
-				e2.printStackTrace();
-			} catch (IllegalAccessException e3) {
-				// TODO Auto-generated catch block
-				e3.printStackTrace();
-			}
-			
-			project.setName( key );
-			
-			MainApp.addProject( key , project );
-			
-			update();
-			
-			mainFrame.getProjectTree().selectProject( key );
-		}
 	}
 	
 	/**
@@ -211,15 +123,6 @@ public class MainApp {
 		WorkspaceHandler.create( reference );
 		
 		mainFrame = new MainFrame();
-	}
-	
-	/**
-	 * Return the number of projects in application
-	 * 
-	 * @return
-	 */
-	public static int getProjectCount() {
-		return projectMap.size();
 	}
 	
 	private static void update() {
