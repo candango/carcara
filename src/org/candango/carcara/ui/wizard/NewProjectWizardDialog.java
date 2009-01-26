84063ab789c6aed2acd1c699f8310aaf0e934e7f
Flavio Garcia <piraz@candango.org>
2009-01-26 18:47:01 +0000

    Adding jTree component. Refers #4.

--- src/org/candango/carcara/ui/wizard/NewProjectWizardDialog.java
+++ src/org/candango/carcara/ui/wizard/NewProjectWizardDialog.java
@@ -1,5 +1,12 @@
 package org.candango.carcara.ui.wizard;
 
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
+import javax.swing.tree.TreeSelectionModel;
+
+
+
 
 public class NewProjectWizardDialog extends AbstractWizardDialog {
 
@@ -12,9 +19,34 @@ public class NewProjectWizardDialog extends AbstractWizardDialog {
 	
 	private static String instruction = "Select a  Wizard";
 	
+	private JTree tree;
+	
 	public NewProjectWizardDialog() {
 		super( title, instruction );
+		createCenterPane();
+		
 	}
 	
-	
+    private void createCenterPane() {
+    	
+    	JPanel pane = new JPanel();
+    	
+    	tree = new JTree();
+        tree.setEditable( true );
+        tree.getSelectionModel().setSelectionMode
+                ( TreeSelectionModel.SINGLE_TREE_SELECTION );
+        
+        tree.setRootVisible( false );
+        
+        tree.setShowsRootHandles( true );
+        
+        //tree.setCellRenderer( new ProjectCellRender() );
+        
+        JScrollPane scrollPane = new JScrollPane( tree );
+        
+        pane.add( scrollPane );
+        
+        addCenterComponent( scrollPane );
+    	
+    }
 }
