358f442b5bb98ce423fd0e777986c5532cc4d304
Flavio Garcia <piraz@candango.org>
2009-01-27 10:59:06 +0000

    Adding header comments.

--- src/org/candango/carcara/ui/wizard/NewProjectWizardDialog.java
+++ src/org/candango/carcara/ui/wizard/NewProjectWizardDialog.java
@@ -1,3 +1,30 @@
+/* NewProjectWizardDialog - NewProjectWizardDialog.java
+ * 
+ * This is a new project wizard dialog. Apears when new project is fired in
+ * application menu.
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * This product includes software developed by the Fusebox Corporation 
+ * (http://www.fusebox.org/).
+ * 
+ * The Original Code is Carcara "a Candango Modeling tool to create 
+ * myFuses/iflux applications" part .
+ * 
+ * The Initial Developer of the Original Code is Flavio Goncalves Garcia.
+ * Portions created by Flavio Goncalves Garcia are Copyright (C) 2008 - 2009.
+ * All Rights Reserved.
+ * 
+ * Contributor(s): Flavio Goncalves Garcia.
+ */
 package org.candango.carcara.ui.wizard;
 
 import javax.swing.JPanel;
@@ -7,7 +34,17 @@ import javax.swing.tree.TreeSelectionModel;
 
 
 
-
+/**
+ * This is a new project wizard dialog. Apears when new project is fired in
+ * application menu.
+ *
+ * @author     Flavio Goncalves Garcia <flavio.garcia at candango.org>
+ * @copyright  Copyright (c) 2008 - 2009 Candango Open Source Group
+ * @link       http://www.candango.org/myfuses
+ * @license    http://www.mozilla.org/MPL/MPL-1.1.html  MPL 1.1
+ * @version    SVN: $Id: Project.java 23 2008-12-07 02:54:38Z flavio.garcia $
+ * @since 0.0.1
+ */
 public class NewProjectWizardDialog extends AbstractWizardDialog {
 
 	/**
@@ -24,7 +61,6 @@ public class NewProjectWizardDialog extends AbstractWizardDialog {
 	public NewProjectWizardDialog() {
 		super( title, instruction );
 		createCenterPane();
-		
 	}
 	
     private void createCenterPane() {
@@ -50,3 +86,4 @@ public class NewProjectWizardDialog extends AbstractWizardDialog {
     	
     }
 }
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
