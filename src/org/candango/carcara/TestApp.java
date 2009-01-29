d25a25d208e83308aa3addb69d36ce4a08a0498a
Flavio Garcia <piraz@candango.org>
2009-01-29 16:24:46 +0000

    Adding test app.

---
+++ src/org/candango/carcara/TestApp.java
@@ -0,0 +1,46 @@
+package org.candango.carcara;
+
+import org.candango.carcara.engine.AbstractDatabaseLoader;
+import org.candango.carcara.engine.DaoBuilder;
+import org.candango.carcara.engine.DatabaseLoader;
+import org.candango.carcara.engine.mysql.MysqlDaoBuilder;
+import org.candango.carcara.engine.mysql.MysqlDatabaseConfiguration;
+
+public class TestApp {
+
+	
+	public static void main( String[] args ) {
+		
+		//MainFrame frame = new MainFrame();
+		
+		DatabaseLoader loader = AbstractDatabaseLoader.getLoader( 
+				
+				AbstractDatabaseLoader.MYSQL_DATABASE );
+		
+		MysqlDatabaseConfiguration configuration = (MysqlDatabaseConfiguration)loader.getConfiguration();
+		
+		//configuration.setSchema( "intranet" );
+		
+		configuration.setHost( "localhost" );
+		
+		configuration.setDatabase( "mysql" );
+		
+		configuration.setUser( "root" );
+		
+		configuration.setPassword( "rootpass" );
+		
+		loader.connect( configuration );
+		
+		loader.doLoad( configuration );
+		
+		loader.disconnect();
+		
+		DaoBuilder builder = new MysqlDaoBuilder();
+		
+		builder.setPath( "./" );
+		
+		builder.build( configuration, loader );
+		
+	}
+	
+}
