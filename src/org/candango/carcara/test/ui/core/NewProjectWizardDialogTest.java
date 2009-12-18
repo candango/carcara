9c8fc6036cf5dac6f629188c7535b97fa02cc779
Flavio Garcia <piraz@candango.org>
2009-12-18 02:01:56 +0000

    Adding new project wizard dialog test. Refers #4.

---
+++ src/org/candango/carcara/test/ui/core/NewProjectWizardDialogTest.java
@@ -0,0 +1,14 @@
+package org.candango.carcara.test.ui.core;
+
+import org.candango.carcara.ui.core.NewProjectWizardDialog;
+
+public class NewProjectWizardDialogTest {
+	
+	public static void main( String[] args ) {
+		
+		NewProjectWizardDialog newFrame = new NewProjectWizardDialog();
+		
+		newFrame.setVisible( true );
+	}
+	
+}
