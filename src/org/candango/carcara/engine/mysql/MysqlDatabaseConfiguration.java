5ecc4e1ff4b0725c698a8691f0c134b3b7b14e3a
Flavio Garcia <piraz@candango.org>
2017-10-08 02:40:12 -0400

    Renamed jdbc driver name.
    Also fixed jdbc connection errro complaining about timezone.

--- src/org/candango/carcara/engine/mysql/MysqlDatabaseConfiguration.java
+++ src/org/candango/carcara/engine/mysql/MysqlDatabaseConfiguration.java
@@ -6,13 +6,16 @@ public class MysqlDatabaseConfiguration extends AbstractDatabaseConfiguration {
 
 	public MysqlDatabaseConfiguration() {
 		super();
-		setDriver( "com.mysql.jdbc.Driver" );
+		setDriver( "com.mysql.cj.jdbc.Driver" );
 		setPort( "3306" );
 	}
 	
 	public String getUrl() {
+		// Added serverTimeZone explicitly to use UTC time zone
+		// @see https://stackoverflow.com/questions/26515700/mysql-jdbc-driver-5-1-33-time-zone-issue
 		String url = "jdbc:mysql://" + getHost() + ":" + getPort() + "/" + 
-			getDatabase();
+			getDatabase() + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&" +
+				"serverTimezone=UTC";
 		return url;
 	}
 
