dca736813fd36aac3a6cce7ccd59ce06a5537c79
Benhur Marques <benhurmarques@gmail.com>
2017-11-28 23:13:46 -0200

    Fixed abstract mysql dao template

--- src/org/candango/carcara/TestApp.java
+++ src/org/candango/carcara/TestApp.java
@@ -8,39 +8,39 @@ import org.candango.carcara.engine.mysql.MysqlDatabaseConfiguration;
 
 public class TestApp {
 
-	
-	public static void main( String[] args ) {
-		
-		//MainFrame frame = new MainFrame();
-		
-		DatabaseLoader loader = AbstractDatabaseLoader.getLoader( 
-				
-				AbstractDatabaseLoader.MYSQL_DATABASE );
-		
-		MysqlDatabaseConfiguration configuration = (MysqlDatabaseConfiguration)loader.getConfiguration();
-		
-		//configuration.setSchema( "intranet" );
-		
-		configuration.setHost( "localhost" );
-		
-		configuration.setDatabase( "acertofacil" );
-		
-		configuration.setUser( "root" );
-		
-		configuration.setPassword( "" );
-		
-		loader.connect( configuration );
-		
-		loader.doLoad( configuration );
-		
-		loader.disconnect();
-		
-		DaoBuilder builder = new MysqlDaoBuilder();
-		
-		builder.setPath( "/home/fpiraz/source/acertofacil/acertofacil/" );
-		
-		builder.build( configuration, loader );
-		
-	}
-	
+
+    public static void main(String[] args) {
+
+        //MainFrame frame = new MainFrame();
+
+        DatabaseLoader loader = AbstractDatabaseLoader.getLoader(
+
+                AbstractDatabaseLoader.MYSQL_DATABASE);
+
+        MysqlDatabaseConfiguration configuration = (MysqlDatabaseConfiguration) loader.getConfiguration();
+
+        //configuration.setSchema( "intranet" );
+
+        configuration.setHost("localhost");
+
+        configuration.setDatabase("acertofacil");
+
+        configuration.setUser("root");
+
+        configuration.setPassword("");
+
+        loader.connect(configuration);
+
+        loader.doLoad(configuration);
+
+        loader.disconnect();
+
+        DaoBuilder builder = new MysqlDaoBuilder();
+
+        builder.setPath("/Users/Charline/java/classes/");
+
+        builder.build(configuration, loader);
+
+    }
+
 }
