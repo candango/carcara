d6ecbe093bbb25259a2722e53c0347bd88ffbc2a
Flavio Garcia <piraz@candango.org>
2018-03-16 22:33:42 -0400

    Added initial application structure with the composer meta.
    Refs: #1

---
+++ src/Carcara/Application.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Carcara (http://carcara.candango.org)
+ *
+ * @link      http://github.com/candango/carcara
+ * @copyright Copyright (c) 2018 Flavio Garcia
+ * @license   https://www.apache.org/licenses/LICENSE-2.0  Apache-2.0
+ */
+
+namespace Candango\Carcara
+{
+    class Application
+    {
+
+    }
+}
