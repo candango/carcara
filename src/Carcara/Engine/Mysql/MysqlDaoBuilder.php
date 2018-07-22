74b202e0c946bcff05917b0b9821ba42ac1cb85c
Flavio Garcia <piraz@candango.org>
2018-07-22 12:57:06 -0400

    Introduced msyql dao builder to the project.
    Refs: #7

---
+++ src/Carcara/Engine/Mysql/MysqlDaoBuilder.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Carcara (http://carcara.candango.org)
+ *
+ * @link      http://github.com/candango/carcara
+ * @copyright Copyright (c) 2018 Flavio Garcia
+ * @license   https://www.apache.org/licenses/LICENSE-2.0  Apache-2.0
+ */
+
+namespace Candango\Carcara\Engine\Mysql {
+
+    use Candango\Carcara\Engine\AbstractDaoBuilder;
+    use Candango\Carcara\Engine\AbstractDatabaseLoader;
+    use Candango\Carcara\Model\Database\Field;
+    use Candango\Carcara\Model\Database\Table;
+
+    class MysqlDaoBuilder extends AbstractDaoBuilder
+    {
+
+    }
+}
