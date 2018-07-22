8030312e36190e926a64bdc7545ed94c2f9b77a6
Flavio Garcia <piraz@candango.org>
2018-07-22 12:57:32 -0400

    Introduced initial lexicon to the application.
    Refs: #1 #7

---
+++ src/Carcara/Lexicon.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Carcara (http://carcara.candango.org)
+ *
+ * @link      http://github.com/candango/carcara
+ * @copyright Copyright (c) 2018 Flavio Garcia
+ * @license   https://www.apache.org/licenses/LICENSE-2.0  Apache-2.0
+ */
+
+namespace Candango\Carcara {
+
+    use Candango\Carcara\Model\Conf;
+    use Candango\Carcara\Model\Database\Table;
+
+    class Lexicon
+    {
+        /**
+         * Returns a camel case formatted entity name.
+         *
+         * The entity name will be separated by.
+         *
+         *   - blank space " "
+         *   - undescore "_"
+         *
+         * @param string $entity
+         * @return string
+         */
+        public static function getEntityName($entity)
+        {
+            $entityName = "";
+            $entityX = explode("_", str_replace(" ", "_", $entity));
+            foreach ($entityX as $part) {
+                $entityName += ucfirst(strtolower($part));
+            }
+            return $entityName;
+        }
+
+        /**
+         * Returns a camel case formatted attribute name.
+         *
+         * It is the same as getEntityName but the first letter is changed to
+         * lower case.
+         *
+         * @param string $attribute
+         * @return string
+         */
+        public static function getAttributeName($attribute)
+        {
+            return lcfirst(self::getEntityName($attribute));
+        }
+
+        /**
+         * Returns the table entity suffix composed by the entity name of conf
+         * identifier and entity name of table name.
+         *
+         * @param Conf $conf
+         * @param Table $table
+         * @return string
+         */
+        public static function getTableEntitySuffix(Conf $conf, Table $table)
+        {
+            return sprintf("%s%s", self::getEntityName($conf->getIdentifier()),
+                self::getEntityName($table->getName()));
+        }
+    }
+}
