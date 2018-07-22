c0c9dd90fb2affc616652a70a0dfa953c0893161
Flavio Garcia <piraz@candango.org>
2018-07-21 23:50:21 -0400

    Introduced initial dao builder structure.
    Refs: #7

---
+++ src/Carcara/Engine/AbstractDaoBuilder.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Carcara (http://carcara.candango.org)
+ *
+ * @link      http://github.com/candango/carcara
+ * @copyright Copyright (c) 2018 Flavio Garcia
+ * @license   https://www.apache.org/licenses/LICENSE-2.0  Apache-2.0
+ */
+
+namespace Candango\Carcara\Engine {
+
+
+    use Candango\Carcara\Model\Database\Table;
+    use Candango\Carcara\Model\Conf;
+
+    abstract class AbstractDaoBuilder implements DaoBuilder
+    {
+        public function build(DatabaseLoader $loader)
+        {
+            // TODO: Implement build() method.
+        }
+
+
+    }
+
+}
---
+++ src/Carcara/Engine/DaoBuilder.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Carcara (http://carcara.candango.org)
+ *
+ * @link      http://github.com/candango/carcara
+ * @copyright Copyright (c) 2018 Flavio Garcia
+ * @license   https://www.apache.org/licenses/LICENSE-2.0  Apache-2.0
+ */
+
+namespace Candango\Carcara\Engine {
+
+    interface DaoBuilder
+    {
+        public function build(DatabaseLoader $loader);
+    }
+
+}
