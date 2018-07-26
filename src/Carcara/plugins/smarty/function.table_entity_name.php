c4beb53ea6674a15093b2bc84cc6e810c9c60a6a
Flavio Garcia <piraz@candango.org>
2018-07-25 23:22:34 -0400

    Renamed function to table entity name.
    Refs: #7

--- src/Carcara/plugins/smarty/function.table_entity_name.php
+++ src/Carcara/plugins/smarty/function.table_entity_name.php
@@ -12,7 +12,7 @@
  * @param $smarty Smarty
  * @return string
  */
-function smarty_function_table_entity_suffix($params, &$smarty) {
+function smarty_function_table_entity_name($params, &$smarty) {
     $conf = $smarty->getTemplateVars("conf");
     $table = $params['table'];
     return Candango\Carcara\Lexicon::getTableEntitySuffix($conf, $table);
